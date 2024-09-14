f5be6137ee7ca32ff56ac468d7e30d9639888dee
anuj22 <anuj22@vt.edu>
2024-09-14 13:11:35 -0400

    test

--- src/Popup.js
+++ src/Popup.js
@@ -7,7 +7,7 @@ const Popup = ({ selectedDay, closePopup }) => {
       <div className="popup-content">
         <h3>Selected Day: {selectedDay}</h3>
         <h2>Item: </h2>
-        <h2>Cal: </h2>
+        <h2>Cal:{selectedDay} </h2>
         <button onClick={closePopup}>Close</button>
       </div>
     </div>
